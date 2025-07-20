a180282f6ee930d5ea046411d132777463082d6d
Quang Dang <>
2025-07-20 21:30:14 +0900

    Fix warning

--- app/src/main/java/com/example/myapplication/BadCodeExample.java
+++ app/src/main/java/com/example/myapplication/BadCodeExample.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
+import java.util.logging.Logger;
 
 public class BadCodeExample {
+    private static final Logger logger = Logger.getLogger(BadCodeExample.class.getName());
+
     public static void main(String[] args) {
         System.out.println("Start");
         BadCodeExample example = new BadCodeExample();
@@ -10,11 +13,11 @@ public class BadCodeExample {
 
     public void doSomething(int value) {
         if (value == 0) {
-            System.out.println("Cannot divide by zero.");
+            logger.warning("Cannot divide by zero.");
         } else if (value == 5) {
-            System.out.println("Five");
+            logger.warning("Five");
         } else {
-            System.out.println("Value: " + value);
+            logger.warning("Value: " + value);
         }
     }
 
